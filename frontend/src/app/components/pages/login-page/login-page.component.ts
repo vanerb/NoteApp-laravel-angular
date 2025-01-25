09dbc841055108f74e5fcd90cb8c9aa66b0e92ba
vanerb <71894451+vanerb@users.noreply.github.com>
2025-01-25 13:38:52 +0100

    Login modal added

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthServiceService } from '../../../services/auth-service.service';
 import { Router } from '@angular/router';
+import { ModalService } from '../../../services/modal.service';
+import { ModalComponentComponent } from '../../general/modal-component/modal-component.component';
 
 @Component({
   selector: 'app-login-page',
@@ -9,14 +11,15 @@ import { Router } from '@angular/router';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
- form!: FormGroup;
+  form!: FormGroup;
 
 
 
   constructor(
     private readonly authService: AuthServiceService,
-    private readonly router: Router
-  ) {}
+    private readonly router: Router,
+    private readonly modalService: ModalService
+  ) { }
 
   ngOnInit() {
     this.formBuilder();
@@ -39,10 +42,25 @@ export class LoginPageComponent {
       next: (response) => {
         this.authService.saveToken(response.data.token)
         this.authService.saveUser(response.data.user)
-        window.location.href = window.location.origin +"/"
+        window.location.href = window.location.origin + "/"
       },
       error: (err) => {
         console.error('Error al crear el producto:', err);
+        this.modalService.openModal(ModalComponentComponent, {
+          data: {
+            title: 'Error',
+            content: 'El usuario o contraseña no son correctos.',
+            isAction: false,
+            actionName: 'Aceptar',
+            acceptAction: () => {
+
+            },
+            cancelAction: () => {
+              this.modalService.closeModal()
+            }
+
+          },
+        });
       },
     });
   }
